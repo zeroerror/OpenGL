a5ade245100ef38194df84242baa10f0cf06f0ae
zh1668118834 <1668118834@qq.com>
2023-03-07 19:56:36 +0800

    引入glew

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +20,11 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "ERROR" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -39,5 +46,6 @@ int main(void)
 	}
 
 	glfwTerminate();
+
 	return 0;
 }
