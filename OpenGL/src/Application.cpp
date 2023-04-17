0a220ab15c28e3ba7d1c2952505384e449769862
zh1668118834 <1668118834@qq.com>
2023-04-17 21:38:55 +0800

    <refactor> 画出2个三角形

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,11 +1,29 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
 
-static unsigned int CompileShader(const int type, const std::string &source)
+// 从文件中读取 Shader 代码
+static std::string ReadShaderFromFile(const char* filepath)
+{
+	std::ifstream stream(filepath);
+	if (!stream.is_open()) {
+		std::cout << "无法打开文件 " << filepath << std::endl;
+		return "";
+	}
+
+	std::stringstream buffer;
+	buffer << stream.rdbuf();
+
+	return buffer.str();
+}
+
+static unsigned int CompileShader(const int type, const std::string& source)
 {
 	unsigned int id = glCreateShader(type);
-	const char *src = source.c_str();
+	const char* src = source.c_str();
 	glShaderSource(id, 1, &src, nullptr);
 	glCompileShader(id);
 
@@ -16,10 +34,10 @@ static unsigned int CompileShader(const int type, const std::string &source)
 	{
 		int len;
 		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
-		char *msg = (char *)alloca(len * sizeof(char));
+		char* msg = (char*)alloca(len * sizeof(char));
 		glGetShaderInfoLog(id, len, &len, msg);
-		std::cout << "缂栬瘧閿欒" << (type == GL_VERTEX_SHADER ? "=銆嬮《鐐圭潃鑹插櫒" : "鐗囧厓鐫�鑹插櫒")
-				  << std::endl;
+		std::cout << "Compile Error=> " << (type == GL_VERTEX_SHADER ? "VertexShader" : "FragmentShder")
+			<< std::endl;
 		std::cout << msg << std::endl;
 		glDeleteShader(id);
 		return 0;
@@ -28,7 +46,7 @@ static unsigned int CompileShader(const int type, const std::string &source)
 	return id;
 }
 
-static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	unsigned int program = glCreateProgram();
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
@@ -47,7 +65,7 @@ static unsigned int CreateShader(const std::string &vertexShader, const std::str
 
 int main(void)
 {
-	GLFWwindow *window;
+	GLFWwindow* window;
 
 	/* Initialize the library */
 	if (!glfwInit())
@@ -69,36 +87,30 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] = {
-		-0.2f, -0.2f,
-		0.2f, 0.2f,
-		0.2f, -0.2f};
+	float positions[12] = {
+		-0.5f,
+		-0.5f,
+		0.5f,
+		-0.5f,
+		0,
+		0.5f,
+		-1.0f,
+		0.5f,
+		1.0f,
+		0.5f,
+		0.5f,
+	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(0.66, 0.33, 0.33, 0.33);\n"
-		"}\n";
+	std::string vertexShader = ReadShaderFromFile("D:/dev/opengl/OpenGL/src/VertexShader.txt");
+	std::string fragmentShader = ReadShaderFromFile("D:/dev/opengl/OpenGL/src/FragmentShader.txt");
 
 	unsigned int shader = CreateShader(vertexShader, fragmentShader);
 	glUseProgram(shader);
@@ -110,6 +122,7 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 3, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
